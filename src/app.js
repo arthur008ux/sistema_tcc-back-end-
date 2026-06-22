0b90e4abf00952416d334a6184e70677f2127b3a
Arthur <chefinh35@gmail.com>
2026-06-22 19:23:43 -0300

    FOI

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // ======================
 app.use(cors({
     origin: [
-        "http://127.0.0.1:5500",
+        "https://front-tcc-eight.vercel.app/",
         "http://localhost:5500"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
