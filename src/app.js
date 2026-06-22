d6f802bbf49ceba53885a45560e6e0f369818450
Arthur <chefinh35@gmail.com>
2026-06-22 19:45:28 -0300

    vaaaaaaaaaai

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // ======================
 app.use(cors({
     origin: [
-        "https://tcc-front-liart.vercel.app/",
+        "https://tccc-front-nine.vercel.app/",
         "http://localhost:5500"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
